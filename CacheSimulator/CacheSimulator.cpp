7bfa4ea15aadb2490b83c67406eb4cb99a2f90c6
smitant <smitant563@gmail.com>
2017-04-26 18:03:16 -0500

    need to debug.

--- CacheSimulator/CacheSimulator.cpp
+++ CacheSimulator/CacheSimulator.cpp
@@ -10,20 +10,18 @@ struct cacheset {
 
 //output variables
 int writeBacks, writeThroughs, hits, misses;
+bool findHit(cacheset * cache, string tag, unsigned int indexBits);
 
 int main()
 {
-	
-
 	FILE * outFile;
 	FILE * parameterFile;
 	FILE * accessesFile;
-	
+
 	string stringLine;
 	int directMapped, offsetBits, numIndexBits;
 	char * allocationPolicy = new char[2];
 	char * writePolicy = new char[2];
-
 
 	parameterFile = fopen("parameters.txt", "r");
 	//parameterFile.open("parameters.txt");
@@ -40,11 +38,11 @@ int main()
 		fclose(parameterFile);
 	}
 	else {
-		fprintf(outFile,"Unable to open parameters file");
+		fprintf(outFile, "Unable to open parameters file");
 		return -1;
 	}
 	//build cache based on parameters
-	
+
 	cacheset * cache1 = new cacheset[pow(2.0, numIndexBits)];
 	cacheset * cache2 = NULL;
 	if (directMapped == 2) {
@@ -61,12 +59,12 @@ int main()
 	//access file processing
 	if (accessesFile)
 	{
-		while (fscanf(accessesFile, "%c %x\n", &readOrWrite, &addressLine)==2) {
+		while (fscanf(accessesFile, "%c %x\n", &readOrWrite, &addressLine) == 2) {
 			//shift the address line over by the offset amount. Offset doesnt matter to us
 			addressLine = addressLine >> offsetBits;
 			unsigned int indexBits = (addressLine << (32 - numIndexBits)) >> (32 - numIndexBits);
 			string tag = to_string(addressLine >> numIndexBits);
-			
+
 
 			if (findHit(cache1, tag, indexBits) == true) {
 				hits++;
@@ -87,8 +85,16 @@ int main()
 						//we know we have a miss, so therefor no tags match
 						//THIS IS THE LAST PORTION WE NEED BEFORE ITS FINISHED.
 						//dont forget to change the dirty bit to 0!!!!
-						if ()
-							
+						writeBacks++;
+						if (cache2 != NULL) {
+							if (cache1[indexBits].firstTagReplace == 0) {
+								cache1[indexBits].dirtyBit = 0;
+							}
+							else {
+								cache2[indexBits].dirtyBit = 0;
+							}
+						}
+
 					}
 					//miss occured, if cache2 == null replace in 
 					if (allocationPolicy == "wa") {
@@ -140,7 +146,7 @@ int main()
 						//set dirtybit back to 0
 						cache1[indexBits].dirtyBit = 0;
 					}
-					
+
 				}
 			}
 
@@ -150,10 +156,15 @@ int main()
 		fclose(accessesFile);
 	}
 	else {
-		cout << "Unable to open accesses file";
+		fprintf(outFile, "Unable to open accesses file");
 		return -1;
 	}
-    return 0;
+
+	//write the variables to the outfile
+	double hitRate = hits / misses;
+	fprintf(outFile, "hits: %d\nmisses: %d\nhrate: %f\nwb: %d\nwt: %d", hits, misses, hitRate, writeBacks, writeThroughs);
+	fcloseall();
+	return 0;
 }
 
 //This method looks through a cache and determines if we hit or miss.
@@ -167,6 +178,6 @@ bool findHit(cacheset * cache, string tag, unsigned int indexBits) {
 			return true;
 		}
 	}
-	
+
 }
 
